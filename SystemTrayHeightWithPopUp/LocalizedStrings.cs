fa6471dee653bd2f711fe9b173ddec78166ade6d
shamimWP <reza@ipvision-inc.com>
2016-04-04 12:43:05 +0600

    first commit

---
+++ SystemTrayHeightWithPopUp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SystemTrayHeightWithPopUp.Resources;
+
+namespace SystemTrayHeightWithPopUp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ SystemTrayHeightWithPopUp/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media;
+using System.Windows.Controls.Primitives;
+
+namespace SystemTrayHeightWithPopUp
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        double systemTrayHeight = 0;
+        Popup popUp;
+        public MainPage()
+        {
+            InitializeComponent();
+            popUp = new Popup();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                GeneralTransform gt = LayoutRoot.TransformToVisual(Application.Current.RootVisual as UIElement);
+                Point offset = gt.Transform(new Point(0, 0));
+                systemTrayHeight = offset.Y;
+            }
+            catch (Exception) { }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            SetSystemTray();
+        }
+
+
+        public static void SetSystemTray()
+        {
+            try
+            {
+                SystemTray.BackgroundColor = Color.FromArgb(255, 250, 110, 40);
+                SystemTray.ForegroundColor = Color.FromArgb(120, 245, 245, 245);
+            }
+            catch (Exception) { }
+        }
+
+        private void ShowPopUp_OnClick(object sender, RoutedEventArgs e)
+        {
+            popUp.VerticalOffset = systemTrayHeight;
+            popUp.Child = new PopUp();
+            popUp.IsOpen = true;
+        }
+
+        private void ClosePopUp_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (this.popUp != null && this.popUp.IsOpen)
+                this.popUp.IsOpen = false;
+        }
+
+    }
+}
